a296cec72e616886e8b6de0ddb27a78d80e15925
tochechka89 <rabota20ton@gmail.com>
2024-12-08 16:26:44 +0200

    ver 2

--- js/task-1.js
+++ js/task-1.js
@@ -1,12 +1,11 @@
-function makeTransaction(quantity, pricePerDroid, customerCredits){
+function makeTransaction(quantity, pricePerDroid, customerCredits) {
     const totalPrice = quantity * pricePerDroid;
-    if(totalPrice>customerCredits) {
+    if (totalPrice > customerCredits) {
         return "Insufficient funds!";
     } else {
         return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
     }
 }
-console.log(makeTransaction);
 
 console.log(makeTransaction(5, 3000, 23000)); // "You ordered 5 droids worth 15000 credits!"
 console.log(makeTransaction(3, 1000, 15000)); // "You ordered 3 droids worth 3000 credits!"
